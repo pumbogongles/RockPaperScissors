fcb2abdea2bdbad811dd4987e4b783bbdb851698
Royston Sim <roys.exe@gmail.com>
2023-09-08 15:02:05 +0800

    Reworked .js file to keep score and stop after 5 wins

--- script.js
+++ script.js
@@ -14,14 +14,20 @@ const buttons = document.querySelectorAll('button')
 buttons.forEach(buttonPress => {
     buttonPress.addEventListener('click', () => {
         const playerSelection = buttonPress.innerText
-        console.log("Player Selected " + playerSelection)
+        // console.log("Player Selected " + playerSelection)
+        document.getElementById('divForResults').querySelector('.sampleTextHere').innerHTML = `You Selected: ${playerSelection}`
         const computerSelection = getComputerChoice();
 
         const result = playRound(playerSelection, computerSelection)
-        console.log(result)
+        // console.log(result)
+        document.getElementById('divForResults').querySelector('.sampleText2').innerHTML = `${result}`
 
-        console.log(`Your score is ${playerWinCount}`)
-        console.log(`Computer score is ${computerWinCount}`)
+
+        // console.log(`Your score is ${playerWinCount}`)
+        // console.log(`Computer score is ${computerWinCount}`)
+        document.getElementById('divForResults').querySelector('.sampleText3').innerHTML = `Your score is ${playerWinCount}`
+        document.getElementById('divForResults').querySelector('.sampleText4').innerHTML = `Computer score is ${computerWinCount}`
+
     })
 })
 
@@ -30,9 +36,24 @@ buttons.forEach(buttonPress => {
 
 playerWinCount = 0;
 computerWinCount = 0;
-function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection == computerSelection) {
+
+// if (playerWinCount == 5) {
+//     document.getElementById('divForResults').querySelector('sampleText3').innerHTML = "You're first to 5, YOU WIN";
+//     document.getElementById('divForResults').querySelector('sampleText4').innerHTML = "";
+// }
+// if (computerWinCount == 5) {
+//     document.getElementById('divForResults').querySelector('sampleText3').innerHTML = "The computer's first to 5, YOU LOSE";
+//     document.getElementById('divForResults').querySelector('sampleText4').innerHTML = "";
+// }
+function playRound(playerSelection, computerSelection) {
+    if (computerWinCount == 5) {
+        return `The computer's first to 5, YOU LOSE`;
+    
+    } else if (playerWinCount == 5) {
+        return `You're first to 5, YOU WIN`;
+
+    } else if (playerSelection == computerSelection) {
         return "Same choices. Draw!"
     } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
         playerWinCount += 1
@@ -55,5 +76,6 @@ function playRound(playerSelection, computerSelection) {
     } else {
         return "Woah, how did we get here? Invalid game, try again"
     }
+
 }
 
